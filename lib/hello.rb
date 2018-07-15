406834a11a63041f3f42c4d762d634e479840395
Hari Govind <hgs@disroot.org>
2018-07-16 00:03:29 +0530

    Hello uses Greeter

--- lib/hello.rb
+++ lib/hello.rb
@@ -1,5 +1,7 @@
-# Default is "World"
-#Author: Hari (hari@example.com)
+require 'greeter'
 
+# Default is World
 name = ARGV.first || "World"
-puts "Hello, #{name}!"
+
+greeter = Greeter.new(name)
+puts greeter.greet
